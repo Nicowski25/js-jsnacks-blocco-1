5f6713c6708d9ea572a15e3b5b33d8ca1f4edc2e
Nicowski25 <nicola.faedo0610@gmail.com>
2023-03-14 12:08:30 +0100

    snack2

---
+++ jsnack2/script.js
@@ -0,0 +1,14 @@
+//SNACK 2:L’utente inserisce due parole in successione, con due prompt.Il software stampa prima la parola più corta, poi la parola più lunga.
+
+//inserire prima parola 
+const firstWord = prompt('inserisci prima parola')
+// inserisci seconda parola
+const secondWord = prompt('inserisci seconda parola')
+
+if (firstWord.length > secondWord.length) {
+    console.log(firstWord + 'è la parola piu lunga');
+} else if (firstWord.length < secondWord.length) {
+    console.log(secondWord + 'è la parola piu lunga');
+} else {
+    console.log('le due parole sono lunghe uguali');
+}
