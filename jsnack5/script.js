ff872d3b4d97323e0bd75cf703a12108300b2379
Nicowski25 <nicola.faedo0610@gmail.com>
2023-03-15 12:43:54 +0100

    for loop

--- jsnack5/script.js
+++ jsnack5/script.js
@@ -13,3 +13,16 @@ while(i < 6) {
     }
     i++
 }
+console.log(lista);
+
+let lista2 = []
+
+for (let i = 0; i < 6; i++) {
+    let numeroInserito = Number(prompt('inserisci un numero'))
+    if(numeroInserito % 2 == 0) {
+        console.log('è pari');
+    } else {
+        lista2.push(numeroInserito)
+    }    
+}
+console.log(lista2);
